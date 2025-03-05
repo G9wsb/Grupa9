1f2cbefcf19a2eca066d98ef35dc8d0dbc643906
G9wsb <wrx95293@student.wroclaw.merito.pl>
2025-03-05 21:34:55 +0100

    n

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, name, email) VALUES (1, 'Justyna Szewczyk', 'justyna.szewczyk@email.com');
+INSERT INTO users (id, name, email) VALUES (2, 'Mateusz Jagielski', 'mateusz.jagielski@email.com');
